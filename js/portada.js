4374fc048a14a31bea42bc92635af7924032fa9b
amayadePastors <59912928+amayadePastors@users.noreply.github.com>
2020-01-22 10:05:37 +0100

    Add files via upload

---
+++ js/portada.js
@@ -0,0 +1,19 @@
+// window.onload=iniciar;
+// var VarFotos=new Array("./imagenes/fotoportada.jpg","./imagenes/2.jpg","./imagenes/3.jpg","./imagenes/4.jpg");
+// var VnbCont=0;
+
+// function iniciar(){
+// 	var VitIntervalo=window.setInterval(mostrarImagenes,5000);
+// }
+
+// function mostrarImagenes(){
+// 	if(VnbCont>=VarFotos.length-1)
+// 		VnbCont=0;
+// 	else
+// 		VnbCont+=1;
+// 	document.images[2].src=VarFotos[VnbCont];
+// }	
+
+
+
+
